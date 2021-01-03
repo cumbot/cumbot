07ff3c0f3deadda77d84c1bb3a06f676fc913ce4
Cumbot <cumbot12345@gmail.com>
2021-01-03 15:34:52 -0500

    added pages to .all and added .rename functionality

--- cum.py
+++ cum.py
@@ -13,6 +13,8 @@ import os
 # 3.) Parse for endlines
 
 qf = './quotes.json'
+pages = []
+page_count = 0
 
 #If quotes is empty then append empty dict
 if not os.path.exists(qf):
@@ -43,19 +45,59 @@ def search(filename, tit):
         r = "No quote found with that title."
     return r
 
+
 #Declaring bot looking for ! commands
 bot = commands.Bot(command_prefix=".")
+
+@bot.command(name='rename')
+async def rename(ctx,oldtit, newtit):
+    r = ''
+    with open(qf) as f:
+        data = json.load(f)
+    if newtit in data:
+        r = "Title taken, try again with a new title."
+    else:
+        try:
+            r = data[oldtit]
+            data[newtit] = r
+            del data[oldtit]
+            with open(qf, 'w') as f:
+                json.dump(data,f)
+        except:
+            r ="No quote found with that title."
+    await ctx.channel.send(r)
 
 #List all quotes
 @bot.command(name='all')
 async def all(ctx):
     with open(qf) as f:
         data = json.load(f)
-    pretty = '```'
+    pretty = ''
     for k,v in data.items():
         pretty += k + ': ' + v + '\n'
     pretty += '```'
-    await ctx.channel.send(pretty)
+
+    pages_rows = pretty.split('\n')
+    global pages
+    pages = []
+    mod = 1
+    apnd = '```Page: 1 \n-------\n'
+    pgnum = 2
+    for x in pages_rows:
+        apnd += x + '\n'
+        if mod % 10 == 0:
+            pages.append(apnd + '```')
+            apnd = '```' + 'Page: ' + str(pgnum) + '\n-------\n'
+            pgnum += 1
+        elif mod == len(pages_rows):
+            pages.append(apnd)
+        mod += 1
+    await ctx.channel.send(pages[0])
+    msg = await ctx.channel.history(limit=1).flatten()
+    msg = msg[0]
+    await msg.add_reaction('\U000023EA')
+    await msg.add_reaction('\U000023E9')
+
 
 #Function for adding title + text
 @bot.command(name="qadd")
@@ -76,6 +118,18 @@ async def quote(ctx, tit):
     except ValueError:
         await ctx.channel.send("Please add a title along with a quote. Example: !qadd title message here")
 
+@bot.event
+async def on_reaction_add(reaction,user):
+    global pages
+    global page_count
+    if reaction.count >= 2 and str(reaction.emoji) == '\U000023EA':
+        page_count += -1
+        await reaction.message.edit(content=pages[page_count % len(pages)])
+        
+    elif reaction.count >= 2 and str(reaction.emoji) == '\U000023E9':
+        page_count += 1
+        await reaction.message.edit(content=pages[page_count % len(pages)])
+
 
 #insert your discord bot token here.
-bot.run('')
+bot.run('Nzg4NTAxNjM0NDk2ODU2MTI1.X9kbXw.ktg_XelyoPpeztxKZGRaVWLQxO0')
